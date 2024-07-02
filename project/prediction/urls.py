b11b0effad4d96919308533721b7df812d9ab872
Randeep <hellonme00@gmail.com>
2024-07-02 18:40:35 +0545

    Commit 3

--- project/prediction/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from django.views.decorators.csrf import csrf_exempt
-from prediction import views
-
-urlpatterns=[
-    path('',views.index,name='index'),
-    path('submit/',views.submit,name='submit'),
-]
